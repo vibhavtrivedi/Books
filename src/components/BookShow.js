feb635894b61a4cbe52f535b32eadb1d2a458464
vibhavtrivedi <vibhavkumar86@gmail.com>
2024-01-13 19:49:46 +0530

    Updated Create and Delete feature

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,5 +1,17 @@
-function BookShow() {
-  return <div>BookShow</div>;
+function BookShow({ book, onDelete }) {
+  const handleDelete = () => {
+    onDelete(book.id);
+  };
+  return (
+    <div className="book-show">
+      {book.title}
+      <div className="actions">
+        <button className="delete" onClick={handleDelete}>
+          Delete
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default BookShow;
