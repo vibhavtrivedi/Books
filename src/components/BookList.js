5ee7f21af42b2ae0e0fa675ec99e2757ae4b264e
vibhavtrivedi <vibhavkumar86@gmail.com>
2024-01-22 21:18:47 +0530

    updated all the crud operations using apis

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,9 +1,13 @@
 import BookShow from "./BookShow";
 function BookList({ books, onDelete, onEdit }) {
-  console.log("books", books);
   const renderedBooks = books.map((book) => {
     return (
-      <BookShow key={book.id} book={book} onDelete={onDelete} onEdit={onEdit} />
+      <BookShow
+        key={book?.id}
+        book={book}
+        onDelete={onDelete}
+        onEdit={onEdit}
+      />
     );
   });
   return <div className="book-list">{renderedBooks}</div>;
