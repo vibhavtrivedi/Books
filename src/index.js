fda8bcffc3c70e70972b838c197e113b9ac66649
vibhavtrivedi <vibhavkumar86@gmail.com>
2024-01-23 21:39:52 +0530

    use create context

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDom from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import BooksContext from "./context/books";
 const el = document.getElementById("root");
 const root = ReactDom.createRoot(el);
-root.render(<App />);
+root.render(
+  <BooksContext.Provider value={5}>
+    <App />
+  </BooksContext.Provider>
+);
